df798022fdf7c7442751fa8a72ce14a2898d61c3
Merkulov <merkulovwork@mail.ru>
2026-04-23 12:22:02 +1000

    сохранл

--- practicum_mui/src/list/List.tsx
+++ practicum_mui/src/list/List.tsx
@@ -1,12 +1,35 @@
 import Navbar from "../components/Navbar";
-import BuildingsGrid from "./components/BuildingsGrid";
 import Footer from "../components/Footer";
+import TimeGrid from "./components/TimeGrid";
+import {useEffect, useState} from "react";
 
 function List() {
+
+    const [stats, setStats] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/v1/track/');
+                const json = await response.json();
+
+                // 3. Сохраняем полученные данные в state
+                setStats(json);
+            } catch (error) {
+                console.error("Ошибка при загрузке:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <Navbar active="2"/>
-            <BuildingsGrid/>
+            <TimeGrid/>
             <Footer/>
         </div>
     );
--- practicum_mui/src/list/components/TimeGrid.tsx
+++ practicum_mui/src/list/components/TimeGrid.tsx
@@ -3,7 +3,7 @@ import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import { ruRU } from '@mui/x-data-grid/locales';
 import Container from "@mui/material/Container";
 
-function BuildingsGrid() {
+function TimeGrid() {
   const rows: GridRowsProp = buildings;
   const columns: GridColDef[] = [
     { field: 'Название', headerName: 'Название'},
@@ -25,4 +25,4 @@ function BuildingsGrid() {
    </Container>
    );
 }
-export default BuildingsGrid;
+export default TimeGrid;
