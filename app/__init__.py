8c9bbcd5b71b5a68c132c3f3235551c8da07a85b
Merkulov <merkulovwork@mail.ru>
2026-04-24 14:53:27 +1000

    добавил миграции

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,12 @@ from .routes import title, track, aggregate
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app,
+         origins=["http://localhost:3000"],
+         supports_credentials=True,
+         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+         allow_headers=["Content-Type", "Authorization"]
+         )
     app.config.from_object(DevelopmentConfig)
     app.json.ensure_ascii = False
 
